0afb145ed0063560718922fccb39329c1937bcb9
William-Newson-Student <wine2a0@student.oaisd.org>
2024-10-07 16:53:08 +0000

    learn basics of print commands and variables

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program 
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("my name is William Newson.");
+
+        int age = 17;
+        string greeting = "Hello World";
+        string firstname = "William";
+        string lastname = "Newson";
+        double money = 2.88;
+
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " " + lastname + ". I am "
+         + age + " years old. I spent $" + money + " on a Energy Drink.");
+    }
+}
